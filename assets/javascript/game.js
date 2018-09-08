6ce84e3907678dec30c15a848aa5d6a5268d3b61
btdrizzle <briandrake05@gmail.com>
2018-09-08 10:19:25 -0500

    coding done.  need to change values for attacks and will be finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,66 +1,61 @@
 $(document).ready(function() {
+    //Object with all properties needed for each character
     var champions = {0: {
                         name: "Ezra",
-                        isRebel: true,
                         image: "./assets/images/Ezra.jpg",
                         HP: 100,
                         attackBegin: 10,
                         counterAttack: 20},
                     1: {
                         name: "Ahsoka",
-                        isRebel: true,
                         image: "./assets/images/Ahsoka.jpg",
                         HP: 135,
                         attackBegin: 8,
                         counterAttack: 30},
                     2: {
                         name: "Kanan",
-                        isRebel: true,
                         image: "./assets/images/Kanan.jpg",
                         HP: 120,
                         attackBegin: 9,
                         counterAttack: 25},
                     3: {
                         name: "Chopper",
-                        isRebel: true,
                         image: "./assets/images/Chopper.jpg",
                         HP: 75,
                         attackBegin: 50,
                         counterAttack: 5},
                     4: {
                         name: "GA Thrawn",
-                        isRebel: false,
                         image: "./assets/images/GAThrawn.jpg",
                         HP: 150,
                         attackBegin: 15,
                         counterAttack: 20},
                     5: {
                         name: "Inquisitor",
-                        isRebel: false,
                         image: "./assets/images/Inquisitor.jpg",
                         HP: 125,
                         attackBegin: 8,
                         counterAttack: 15},
                     6: {
                         name: "Agent Kallus",
-                        isRebel: false,
                         image: "./assets/images/Kallus.jpg",
                         HP: 100,
                         attackBegin: 11,
                         counterAttack: 17},
                     7: {
                         name: "Tarkin",
-                        isRebel: false,
                         image: "./assets/images/Tarkin.jpg",
                         HP: 200,
                         attackBegin: 6,
                         counterAttack: 2}
     };
+    //Playlist used for background music
     var playlist = ["../assets/audio/Alliance.mp3",
                     "../assets/audio/Glory_of_the_empire.mp3",
                     "../assets/audio/Yodas_guidance.mp3",
                     "../assets/audio/Imperial_Inquisition.mp3"];
-
+                    
+    //Using this function to play event sounds           
     function sound(src) {
         this.sound = document.createElement("audio");
         this.sound.src = src;
@@ -75,13 +70,20 @@ $(document).ready(function() {
             this.sound.pause();
         }
     }
-    bgAudio();
+    //Click Sound
+    var clicky = new sound("../assets/audio/heavy_throw_switch.mp3");
+    var force = new sound("../assets/audio/Strongwiththeforce.mp3");
+    var darkside = new sound("../assets/audio/darkside.mp3");
+
+    //This sound function is for background music
     function bgAudio () {
         var playlist_index;
         playlist_index = 0;
         audio = new Audio();
         audio.src = playlist[0];
         audio.loop = false;
+        audio.volume = 0.25;
+        audio.muted = true;
         audio.play();
         audio.addEventListener('ended', function() {
             switchTrack();
@@ -99,16 +101,23 @@ $(document).ready(function() {
     }
     bgAudio();
 
+    function togglePlay() {
+        if(audio.muted) {
+            audio.muted = false;
+        }else{
+            audio.muted = true;
+        }
+    }
 
-    var click = new sound("../assets/audio/heavy_throw_switch.mp3");
-    
-    playGame();
-    function playGame() {
+    $('#audioButton').on('click', function() {
+        togglePlay();
+    });
 
-
-        for (var i = 0; i < 8; i++) {
+    //This function creates character tiles and gives them attributes and classes
+    function makeCharacters() {
+        for (let i = 0; i < 8; i++) {
             var champDiv = $("<div>");
-            champDiv.addClass("tile d-inline-block border border-dark bg-dark m-2 text-white");
+            champDiv.addClass("tile d-inline-block border border-dark bg-dark m-2 text-white btn");
             champDiv.append($("<h4>").text(champions[i]["name"]));
             champDiv.append($("<img>", {"src": champions[i]["image"]})).addClass("img-fluid");
             champDiv.append($("<p>").addClass("lead font-weight-bold hp-text")
@@ -121,59 +130,29 @@ $(document).ready(function() {
             }
             $('#choose').append(champDiv); 
         }
+    }
+    //Variables needed to make the game work, especially with calculations
 
-        $(".tile").on("click", function() {
-            if( $('#champion').is(':empty')) {
-                click.play();
-                $('#game-info-text').empty();
-                $(this).attr("data-champOrAdversary", "champion");
-
-                $('#champion').append($(this));
-                $('#choose-text').text("Choose your adversary!");
-            }
-            else if( $('#adversary').is(':empty')) {
-                click.play();
-                $('#game-info-text').empty();
-                $(this).attr("data-champOrAdversary", "adversary");                   
-                $('#adversary').append($(this));
-            }
-        });
-        $('#attack').on("click", function() {
-            if( $('#champion').is(':empty')) {
-                $("#game-info-text").text("You have not selected a champion!");
-            }
-            else if( ($('#champion').is(':empty') === false) && $('#adversary').is(':empty')) {
-                $("#game-info-text").text("You have not selected an adversary!");
-            }
-            else {
-                clickFunction();
-            }
-        });
-        function playAgainButton() {
-            var playAgainButton = $('<button>');
-            playAgainButton.text('Click to play again!');
-
-            $('#game-over-button').append(playAgainButton);
-            playAgainButton.on('click', function() {
-                $('#champion').empty();
-                $('#adversary').empty();
-                $('#choose').empty();
-                $('#game-info-text').empty();
-                $('#game-end-text').empty();
-                $('#game-over-text').empty();
-                $('#game-over-button').empty();
-                $('#choose-text').text('Choose your champion!');
-                playGame();
-            });
-
+    var goodGuy,badGuy,goodHP,badHP,goodAttack,badCounter,goodName,badName,newText,newBadHP,newGoodHP, gameOver; 
+    
+    //This is what happens when the "Attack" button is clicked and the game is played
+    function clickFunction() {
+        console.log('asdf')
+        $('#adversary').off();
+        if(gameOver === true) {
+            return;
         }
-
-        var goodGuy,badGuy,goodHP,badHP,goodAttack,badCounter,goodName,badName,newText,newBadHP,newGoodHP, gameOver;
-        
-        function clickFunction() {
-            if(gameOver === true) {
-                return
-            }
+        else if($('#champion').is(':empty')) {
+            clicky.play();
+            $("#game-info-text").text("You have not selected a champion!");
+        }
+        else if($('#adversary').is(':empty')) {
+            clicky.play();
+            $('#game-end-text').empty();
+            $("#game-info-text").text("You have not selected an adversary!");
+        }
+        else {
+            clicky.play();
             $('#game-end-text').empty();
             goodGuy = $('[data-champOrAdversary="champion"]');
             badGuy = $('[data-champOrAdversary="adversary"]');
@@ -190,6 +169,8 @@ $(document).ready(function() {
             newGoodHP = parseInt(goodHP) - parseInt(badCounter);
 
             badGuy.attr("data-HP", newBadHP);
+
+
             goodGuy.attr("data-HP", newGoodHP);
 
             goodGuy.find('.lead').text("HP: " + goodGuy.attr("data-HP"));
@@ -203,42 +184,106 @@ $(document).ready(function() {
             goodAttack = parseInt(goodAttack) + parseInt(goodAttack);
 
             goodGuy.attr("data-attackBegin", goodAttack);
-            
-            whatHappensNow();
-            function whatHappensNow() {
-                if(parseInt(badGuy.attr("data-HP")) < 1) {
-                    $('#game-end-text').text("You have defeated an enemy!  The force is strong with you!  Select another!");
-                    $('#adversary').empty();
-                }
+
+            //Analyzes what happened during the click of "Attack" and makes determinations
+            if((newBadHP < 1) && $('#choose').is(':empty')) {
+                $('#game-over-text').text("You have won the game!  You are the chosen one who brings balance to the force!");
+                force.play();
+                gameOver === true;
+                playAgainButton();
+                
+            }
+            else if(newBadHP < 1) {
+                console.log('win')
+                $('#game-end-text').text("You have defeated an enemy!  The force is strong with you!  Select another!");
+                $('#adversary').empty();
                 if(parseInt(goodGuy.attr("data-HP")) < 1) {
+                    $('#game-end-text').empty();
                     $('#game-over-text').text("You have lost the game, padawan!  Click below to play again!  Do or do not, there is no try!");
+                    darkside.play();
                     gameOver === true;
                     playAgainButton();
-                    return
+                    
                 }
-                if((parseInt(badGuy.attr("data-HP")) < 1) && $('#choose').is(':empty')) {
-                    $('#game-over-text').text("You have won the game!  You are the chosen one who brings balance to the force!");
-                    gameOver === true;
-                    playAgainButton();
-                    return
-                }
+            }
+            else if(newGoodHP < 1) {
+                $('#game-over-text').text("You have lost the game, padawan!  Click below to play again!  Do or do not, there is no try!");
+                darkside.play();
+                gameOver === true;
+                playAgainButton();
                 
             }
         }
     }
+    //This cleans the slate for a new game
+    function playAgainButton() {
+        var playAgainButton = $('<button>');
+        playAgainButton.text('Click to play again!');
+        playAgainButton.attr("type","button");
+        playAgainButton.addClass("btn btn-light");
+
+        $('#game-over-button').append(playAgainButton);
+        playAgainButton.on('click', function() {
+            clicky.play();
+            goodGuy === ""
+            badGuy === ""
+            goodHP === ""
+            badHP === ""
+            goodAttack === ""
+            badCounter === ""
+            goodName === ""
+            badName === ""
+            newText === ""
+            newBadHP === ""
+            newGoodHP === ""
+            $('#champion').empty();
+            $('#adversary').empty();
+            $('#choose').empty();
+            $('#game-info-text').empty();
+            $('#game-end-text').empty();
+            $('#game-over-text').empty();
+            $('#game-over-button').empty();
+            $('#choose-text').text('Choose your champion!');
+            gameOver === false;
+            makeCharacters();
+
+            // playGame();
+        });
+
+    }
 
 
-    
+    function playGame() {
+        gameOver === false;
 
+        makeCharacters();
 
+        //Fills "Champion" first, then "Adversary."  Then only "Adversary" until the next game.
+        $("body").on("click", ".tile", function() {
+            if( $('#champion').is(':empty')) {
+                clicky.play();
+                $('#game-info-text').empty();
+                $(this).attr("data-champOrAdversary", "champion");
 
+                $('#champion').append($(this));
+                $('#choose-text').text("Choose your adversary!");
+            }
+            else if( $('#adversary').is(':empty')) {
+                clicky.play();
+                $('#game-info-text').empty();
+                $('#game-end-text').empty();
+                $(this).attr("data-champOrAdversary", "adversary");                   
+                $('#adversary').append($(this));
+            }
+        });
 
+        //What happens when the "Attack" button is clicked
 
+        $('#attack').on("click", function() {
+            clickFunction();
+        });
 
-
-
-
-
-
-
+    }
+    //Play the game for the first time
+    playGame();
 });
